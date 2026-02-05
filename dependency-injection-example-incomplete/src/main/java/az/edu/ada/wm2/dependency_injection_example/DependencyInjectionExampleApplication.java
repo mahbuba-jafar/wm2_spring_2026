ffe43838dc6aa56d7569df8daa84b28410377b50
nsadili <nuraddinsadili93@gmail.com>
2026-02-05 09:48:57 +0400

    Add Lab3 Incomplete project

---
+++ dependency-injection-example-incomplete/src/main/java/az/edu/ada/wm2/dependency_injection_example/DependencyInjectionExampleApplication.java
@@ -0,0 +1,14 @@
+package az.edu.ada.wm2.dependency_injection_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInjectionExampleApplication.class, args);
+
+    }
+
+}
