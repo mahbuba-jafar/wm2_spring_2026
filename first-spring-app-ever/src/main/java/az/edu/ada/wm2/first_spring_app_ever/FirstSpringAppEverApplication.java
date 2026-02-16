8fa147c587add7638ae9320f66fad689e76fe5b3
nsadili <nuraddinsadili93@gmail.com>
2026-02-16 16:54:37 +0400

    Add first-spring-app using RESTController

---
+++ first-spring-app-ever/src/main/java/az/edu/ada/wm2/first_spring_app_ever/FirstSpringAppEverApplication.java
@@ -0,0 +1,24 @@
+package az.edu.ada.wm2.first_spring_app_ever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstSpringAppEverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppEverApplication.class, args);
+	}
+
+//    @RequestMapping(value = "/sayHello", method = RequestMethod.GET)
+    @GetMapping("/sayHello")
+    public String sayHelloToMyLittleFriend(){
+        return "Hello World!";
+    }
+
+}
--- first-spring-app-ever/src/test/java/az/edu/ada/wm2/first_spring_app_ever/FirstSpringAppEverApplicationTests.java
+++ first-spring-app-ever/src/test/java/az/edu/ada/wm2/first_spring_app_ever/FirstSpringAppEverApplicationTests.java
@@ -1,10 +1,10 @@
-package az.edu.ada.wm2.first_spring_mvc;
+package az.edu.ada.wm2.first_spring_app_ever;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FirstSpringMvcApplicationTests {
+class FirstSpringAppEverApplicationTests {
 
 	@Test
 	void contextLoads() {
